5d17350785066890d97e83cfe61c85e10384260f
yuhei_kajima <yuhei_kajima@nuco.co.jp>
2019-12-24 13:52:08 +0900

    updated

--- sakurahubuki.py
+++ sakurahubuki.py
@@ -1,14 +1,31 @@
 import random
 import argparse
+import logging
 
 
-def main(args):
-  MEMBERS = [i for i in range(args.num)]
-  print(args)
+class Player:
+  def __init__(self, index):
+    self.index = index
+
+
+class SakuraCounter:
+  pass
+
+
+class CountResultStore:
+  pass
+
+
+class BestNumberDetector:
+  pass
+
+
+def main(n):
+  PLAYER_INDEXES = [i for i in range(n)]
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='桜吹雪ゲーム攻略プログラム')
-  parser.add_argument('--num', type=int, help='対戦人数')
+  parser.add_argument('--num', type=int, help='対戦人数', required=True)
   args = parser.parse_args()
-  main(args)
+  main(args.num)
