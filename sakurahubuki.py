e0f949eb8ea4ffd606f0e80d42d402ba1ed65fbe
yuhei_kajima <yuhei_kajima@nuco.co.jp>
2019-12-26 15:05:24 +0900

    updated

--- sakurahubuki.py
+++ sakurahubuki.py
@@ -3,19 +3,24 @@ import argparse
 import logging
 
 
+# 識別番号を持ち、自分意外の番号を指定する。
 class Player:
   def __init__(self, index):
     self.index = index
 
 
+# メンバー数を持ち、指定された数に対して対象のメンバーインデックスを返す.
 class SakuraCounter:
-  pass
+  def __init__(self, player_num):
+    self.player_num = player_num
 
 
+# 指定される数を持ち、その数の際の結果をストアしていく。
 class CountResultStore:
   pass
 
 
+# CountResultStoreを配列として受け取り、最も自滅しない数を返す。
 class BestNumberDetector:
   pass
 
